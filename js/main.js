146bc9fd6735293af1e4b4922c992946163ee3d2
QasemMh <kasem200094@gmail.com>
2021-10-04 19:09:04 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//comment 
+//Swiper
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
