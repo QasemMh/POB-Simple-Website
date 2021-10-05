0c4393885734a151fcb9821ce65399a6a919bd16
QasemMh <kasem200094@gmail.com>
2021-10-05 17:07:18 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 //Swiper
-var swiper = new Swiper(".swiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+// var swiper = new Swiper(".swiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+const typed = new Typed("#typed__target", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 70,
+  loop: true,
+  loopCount: 2,
+  backSpeed: 40,
+  smartBackspace: false,
+  backDelay: 1500,
 });
