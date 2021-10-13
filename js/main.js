fc4c66991c7bf095ad847fc782bce6b440195385
QasemMh <kasem200094@gmail.com>
2021-10-13 16:48:40 +0300

    commit 5

--- js/main.js
+++ js/main.js
@@ -59,3 +59,79 @@ if (get_(".grid figure")) {
     $(this).find("span").css({ top: y, left: x });
   });
 }
+
+//theme switcher:
+let closeSwitcher = get_("#hideSwitcher");
+let showSwitcher = get_("#showSwitcher");
+let switcherBody = get_("#switcher-main");
+
+closeSwitcher.addEventListener("click", () => {
+  switcherBody.style.marginLeft = "-500px";
+  showSwitcher.style.marginLeft = "0px";
+});
+
+showSwitcher.addEventListener("click", () => {
+  switcherBody.style.marginLeft = "0px";
+  showSwitcher.style.marginLeft = "-500px";
+});
+
+let colorsBody = get_(".switcher__color");
+let colorsSpan = getAll_(".switcher__color span");
+
+colorsSpan.forEach((item) => {
+  item.style.background = item.dataset.color;
+
+  item.addEventListener("click", function () {
+    document.documentElement.style.setProperty(
+      "--main-color",
+      item.dataset.color
+    );
+
+    //save to localstorage:
+    localStorage.setItem("themeColor", item.dataset.color);
+  });
+});
+
+let theme = getAll_(".switcher__content_theme h3")[0];
+let theme2 = getAll_(".switcher__content_theme h3")[1];
+let dark = true;
+
+theme.addEventListener("click", () => {
+  switchTheme(true);
+});
+theme2.addEventListener("click", () => {
+  switchTheme(false);
+});
+
+if (localStorage.getItem("themeColor") != null) {
+  document.documentElement.style.setProperty(
+    "--main-color",
+    localStorage.getItem("themeColor")
+  );
+}
+
+if (localStorage.getItem("theme") != null) {
+  let isLight = localStorage.getItem("theme") == "light" ? true : false;
+  switchTheme(isLight);
+}
+
+function switchTheme(light) {
+  if (light) {
+    document.documentElement.style.setProperty("--font-color", "#111");
+    document.documentElement.style.setProperty("--main-bg", "#fff");
+    document.documentElement.style.setProperty("--nav-color", "#ddd");
+    document.documentElement.style.setProperty("--border-color", "#ddd");
+    theme.classList.add("active");
+    theme2.classList.remove("active");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.style.setProperty("--font-color", "#fff");
+    document.documentElement.style.setProperty("--main-bg", "#111");
+    document.documentElement.style.setProperty("--nav-color", "#2b2a2a");
+    document.documentElement.style.setProperty("--border-color", "#252525");
+    theme2.classList.add("active");
+    theme.classList.remove("active");
+
+    localStorage.setItem("theme", "dark");
+  }
+}
